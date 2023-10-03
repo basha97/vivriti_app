51e87d35eb42e8d5d4fb5708bdb9bcffce0e1f6d
basha97 <bashaadil2727@gmail.com>
2023-10-03 12:29:04 +0530

    feat: added splash screen

--- lib/app/splash.dart
+++ lib/app/splash.dart
@@ -1,10 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:vivriti_app/core/resources/route_manager.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => Navigator.pushReplacementNamed(context, Routes.homeRoute),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl:
+              'https://cdn-images-1.medium.com/v2/resize:fit:184/1*OmMk5vql6JUhLctEQfYR4w@2x.png',
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+      ),
+    );
   }
 }
--- lib/core/resources/route_manager.dart
+++ lib/core/resources/route_manager.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vivriti_app/app/splash.dart';
+import 'package:vivriti_app/features/home/view/home.dart';
 
 class Routes {
   static const String splashRoute = '/';
+  static const String homeRoute = '/home';
 }
 
 class RouteGenerator {
@@ -10,6 +12,8 @@ class RouteGenerator {
     switch (routeSettings.name) {
       case Routes.splashRoute:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case Routes.homeRoute:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return unDefinedRoute();
     }
